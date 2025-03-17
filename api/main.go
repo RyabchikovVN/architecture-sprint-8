98d9d213d3ba429c84b92beb33ac6033f5c8924a
RyabchikovVN <107054557+RyabchikovVN@users.noreply.github.com>
2025-03-17 07:28:17 +0300

    Update main.go

--- api/main.go
+++ api/main.go
@@ -4,16 +4,34 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"github.com/gorilla/mux"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
 )
+
+// CORSMiddleware добавляет необходимые заголовки для поддержки CORS
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")                            // Разрешаем все источники (для разработки)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")          // Разрешаем методы
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // Разрешаем заголовки
+
+		if r.Method == http.MethodOptions {
+			// Обработка preflight-запросов
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r) // Передаем управление следующему обработчику
+	})
+}
 
 var mySigningKey = []byte("oNwoLQdvJAvRcL89SydqCWCe5ry1jMgq")
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/reports", reportsHandler).Methods("GET")
-	http.ListenAndServe(":8000", r)
+	// Оборачиваем маршрутизатор в middleware CORS
+	http.ListenAndServe(":8000", CORSMiddleware(r))
 }
 
 func reportsHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,20 +51,20 @@ func reportsHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w, "Token ERROR" )
+		fmt.Fprint(w, "Token ERROR!")
 		return
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		if claims["roles"] != "prothetic_user" {
 			w.WriteHeader(http.StatusUnauthorized)
-			fmt.Fprint(w, "Need role is nothing")
+			fmt.Fprint(w, "Target role not found!")
 			return
 		}
-		fmt.Fprint(w, "OK - WELCOME - " + tokenString)
-	} else {		
+		fmt.Fprint(w, "OK - WELCOME!")
+	} else {
 		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w, "Token not valid")
-	}	
+		fmt.Fprint(w, "Token not valid!")
+	}
 
 }
