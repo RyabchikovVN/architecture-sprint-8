f9560e666f9f6b9f7bcc79ce901b98f75707b520
RyabchikovVN <107054557+RyabchikovVN@users.noreply.github.com>
2025-03-19 20:42:19 +0300

    Update main.go

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
@@ -25,47 +26,47 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-var mySigningKey = []byte("oNwoLQdvJAvRcL89SydqCWCe5ry1jMgq")
-
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/reports", reportsHandler).Methods("GET")
-	// Оборачиваем маршрутизатор в middleware CORS
 	http.ListenAndServe(":8000", CORSMiddleware(r))
 }
 
+var publicKey = []byte("\n-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1mxs5cw/bOGQ+PHLFeIwDAUzGhKLDYbNJaqoHjvV/GUCAp/1FF/TDIiEN2fuLLeAgpPZKZSEG+bxXJ7vXXxSNvUjY1etmRvHbhlMN+rJYTQtWLZxsvM/MfYi0b+l20oeGtCBwUa2CKWFssnxBM5L3Ex7bmTKzetivOrFP25ztCd6Bu5RXaZ/KJgJlFoHqw8GkGp6iMb1bXcyOSlndJWYprHfa5XjbBsNmXwZ6y8AM3V8Qb+dzDeA60qmX5RlWQOp80W50QKh7BOBud0JuLewZE4JKiWUsM5Csisc/XZbYQSpWZTgGduqi1taYDOZHSPA/yJUtIqdXiyCwo5P0oIqKwIDAQAB\n-----END PUBLIC KEY-----\n")
+
 func reportsHandler(w http.ResponseWriter, r *http.Request) {
 	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
-		http.Error(w, "Authorization header is required", http.StatusUnauthorized)
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+	pubKey, err := jwt.ParseRSAPublicKeyFromPEM(publicKey)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "ERROR: "+err.Error())
 		return
 	}
 
-	//	ParseRSAPublicKeyFromPEM
-	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			f _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
-		return mySigningKey, nil
+		return pubKey, nil
 	})
 
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w, "Token ERROR! " + err.Error())
+		fmt.Fprint(w, "Unauthorized - err: "+err.Error())
 		return
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		if claims["roles"] != "prothetic_user" {
+		if claims["realm_access"].(map[string]interface{})["roles"].([]interface{})[0] != "prothetic_user" {
 			w.WriteHeader(http.StatusUnauthorized)
-			fmt.Fprint(w, "Target role not found!")
+			fmt.Fprint(w, "Unauthorized")
 			return
 		}
-		fmt.Fprint(w, "OK - WELCOME!")
+		fmt.Fprint(w, "Welcome!")
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w, "Token not valid!")
+		fmt.Fprint(w, "Unauthorized")
 	}
-
 }
